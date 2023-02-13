418979d41e9c02af6a3d78dcc474b50c9d3bfc89
Johnny Bhojwani <johnny_bhojwani@baylor.edu>
2023-02-13 17:17:40 -0600

    initial commit

---
+++ ProcessEmployees.py
@@ -0,0 +1,49 @@
+'''
+The Customer Service Represetatives (CSRs) in the marketing department with a security clearance of 'TS' were able
+to thwart an attack on the server and for that management has decided to reward them with a 10% increase in their salary. 
+To evaluate the impact on the budget, you have been asked to run a report on the employee file and display the results 
+of BEFORE AND AFTER the salary increase. Also calculate the total difference between the old salary and the new
+salary (as shown in the image).
+
+You will create a dictionary that has the full employee name as the key and only their NEW salary as the value.
+
+Iternate through the dictionary to print out their name and thier new salary (as shown in the image)
+'''
+
+import csv
+
+#open the file
+
+
+
+
+#create an empty dictionary
+
+
+#use a loop to iterate through the csv file
+
+
+    #check if the employee fits the search criteria
+
+
+    
+
+print()
+print('=========================================')
+print()
+
+#iternate through the dictionary and print out the key and value as per image
+
+
+
+
+          
+
+print()
+print('=========================================')
+print()
+
+#print out the total difference between the old salary and the new salary.
+
+        
+    
